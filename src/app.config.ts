0b407d63f2c1114c0c8b7df31585abd3012d675b
swlws <979623074@qq.com>
2026-06-07 11:01:59 +0800

    feat: app conifg

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,35 +1,34 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index',
-    'pages/login/index',
-    'pages/home/index',
-    'pages/mine/index'
+    "pages/index/index",
+    "pages/login/index",
+    "pages/home/index",
+    "pages/mine/index",
   ],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: '',
-    navigationBarTextStyle: 'black'
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "",
+    navigationBarTextStyle: "black",
   },
   tabBar: {
-    color: '#999999',
-    selectedColor: '#1677ff',
-    backgroundColor: '#ffffff',
-    borderStyle: 'black',
+    color: "#999999",
+    selectedColor: "#1677ff",
+    backgroundColor: "#ffffff",
+    borderStyle: "black",
     list: [
       {
-        pagePath: 'pages/home/index',
-        text: '主页',
-        iconPath: 'assets/logo.png',
-        selectedIconPath: 'assets/logo.png'
+        pagePath: "pages/home/index",
+        text: "主页",
+        iconPath: "assets/logo.png",
+        selectedIconPath: "assets/logo.png",
       },
       {
-        pagePath: 'pages/mine/index',
-        text: '我的',
-        iconPath: 'assets/logo.png',
-        selectedIconPath: 'assets/logo.png'
-      }
-    ]
+        pagePath: "pages/mine/index",
+        text: "我的",
+        iconPath: "assets/logo.png",
+        selectedIconPath: "assets/logo.png",
+      },
+    ],
   },
-  requiredPrivateInfos: ['getPhoneNumber']
-})
+});
